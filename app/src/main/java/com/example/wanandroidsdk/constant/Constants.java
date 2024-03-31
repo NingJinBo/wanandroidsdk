299195c3f8ee921382145b6f239b35c8af7c80e5
smile drily <690498789@qq.com>
2024-03-31 16:59:05 +0800

    添加工具类

---
+++ app/src/main/java/com/example/wanandroidsdk/constant/Constants.java
@@ -0,0 +1,23 @@
+package com.example.wanandroidsdk.constant;
+
+/**
+ * @auth: njb
+ * @date: 2024/3/31 16:53
+ * @desc: 描述
+ */
+public class Constants {
+    public static final String EasyPlayHostDefault = "https://www.wanandroid.com";
+
+    //获取首页banner列表
+    public static final String BANNER_LIST = "/banner/json";
+
+    public static final String ARTICLE_LIST = "/article/list/{page}/json";
+
+    public static final String HOT_KEY = "/hotkey/json";
+
+    public static final String TREE = "/tree/json";
+
+    public static final String NAVI = "/navi/json";
+
+    public static final String PROJECT_TREE = "/project/tree/json";
+}
