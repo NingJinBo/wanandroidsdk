29128ed153269f8e0da63a1d398e87a9111e2e6c
smile drily <690498789@qq.com>
2024-04-01 00:42:39 +0800

    修改jdk版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,24 +46,24 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
 
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("io.reactivex.rxjava2:rxjava:2.2.12")
-    implementation("com.squareup.retrofit2:retrofit:2.6.2")
-    implementation("com.squareup.retrofit2:adapter-rxjava2:2.6.0")
+    implementation ("io.reactivex.rxjava2:rxjava:2.2.21")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:adapter-rxjava2:2.9.0")
     implementation("com.squareup.retrofit2:converter-scalars:2.4.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.6.2")
-    implementation("com.squareup.okhttp3:okhttp:4.2.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:3.6.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
 }
