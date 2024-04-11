97297bbdf1d830aebdf8e2a26b767468c8d05fdc
smile drily <690498789@qq.com>
2024-04-12 00:00:51 +0800

    修改build、settings配置为kts语法

---
+++ app/build.gradle.kts
@@ -0,0 +1,78 @@
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.mavenPublish)
+}
+
+group = "om.example.wanandroidsdk"
+version = "1.0.0"
+
+
+afterEvaluate {
+    publishing {
+        publications {
+            // Creates a Maven publication called "release".
+            create<MavenPublication>("release") {
+                // Applies the component for the release build variant.\
+                from(components["release"])
+                // You can then customize attributes of the publication as shown below.
+                groupId = "om.example.wanandroidsdk"
+                artifactId = "wanandroidsdk-kts"
+                version = version
+            }
+        }
+    }
+}
+
+android {
+    namespace  = "com.example.wanandroidsdk"
+    compileSdk =  34
+
+    defaultConfig {
+        //applicationId = "com.example.wanandroidsdk"
+        minSdk = 24
+        targetSdk = 34
+/*        versionCode = 1
+        versionName = "1.0"*/
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            //isDebuggable = false
+//            signingConfig = signingConfigs.getByName("release")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.okhttp)
+    implementation(libs.logging.interceptor)
+    implementation(libs.utilcodex)
+    implementation(libs.rxjava)
+    implementation(libs.retrofit)
+    implementation(libs.adapter.rxjava2)
+    implementation(libs.converter.scalars)
+    implementation(libs.converter.gson)
+}
