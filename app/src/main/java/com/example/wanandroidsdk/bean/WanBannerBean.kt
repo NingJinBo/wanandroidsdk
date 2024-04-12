7eef2537c1a4b5a278a4aa5fb8cb415232041926
smile drily <690498789@qq.com>
2024-04-12 19:22:14 +0800

    添加Banner数据类

---
+++ app/src/main/java/com/example/wanandroidsdk/bean/WanBannerBean.kt
@@ -0,0 +1,12 @@
+package com.example.wanandroidsdk.bean
+
+data class WanBannerBean(
+    var desc:String = "",
+    var id:String = "",
+    var imagePath:String = "",
+    var isVisible:Int = 0,
+    var order:Int =0,
+    var title:String = "",
+    var type:String = "",
+    var url:String = ""
+)
