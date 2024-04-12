424d20666d6718cad07b3bde7532298b8b1b7dce
smile drily <690498789@qq.com>
2024-04-12 19:21:26 +0800

    添加请求ApiService类

--- app/src/main/java/com/example/wanandroidsdk/http/api/ApiService.kt
+++ app/src/main/java/com/example/wanandroidsdk/http/api/ApiService.kt
@@ -1,7 +1,7 @@
 package com.example.wanandroidsdk.http.api
 
-import com.example.wanandroidsdk.bean.ArticleListBean
-import com.example.wanandroidsdk.bean.EasyDataBean
+import com.example.wanandroidsdk.bean.WanArticleListBean
+import com.example.wanandroidsdk.bean.WanDataBean
 import com.example.wanandroidsdk.constant.Constants
 import com.example.wanandroidsdk.http.WanHttpResult
 import retrofit2.http.GET
@@ -9,14 +9,14 @@ import retrofit2.http.Path
 
 interface ApiService {
     @GET(Constants.BANNER_LIST)
-    suspend fun getBanner():  WanHttpResult<List<EasyDataBean>>
+    suspend fun getBanner():  WanHttpResult<List<WanDataBean>>
 
     @GET(Constants.ARTICLE_LIST)
-    suspend fun articleList(@Path("page") page:Int): WanHttpResult<ArticleListBean>
+    suspend fun articleList(@Path("page") page:Int): WanHttpResult<WanArticleListBean>
 
     @GET(Constants.HOT_KEY)
-    suspend fun hotKeyList(): WanHttpResult<List<EasyDataBean>>
+    suspend fun hotKeyList(): WanHttpResult<List<WanDataBean>>
     
     @GET(Constants.TREE)
-    suspend fun knowList(): WanHttpResult<List<EasyDataBean>>
+    suspend fun knowList(): WanHttpResult<List<WanDataBean>>
 }
