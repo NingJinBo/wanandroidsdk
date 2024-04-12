4e5bb75aa490f5c899581e3bd9c9811f307c7c28
smile drily <690498789@qq.com>
2024-04-12 19:22:04 +0800

    添加数据基类

---
+++ app/src/main/java/com/example/wanandroidsdk/bean/WanArticleListBean.kt
@@ -0,0 +1,5 @@
+package com.example.wanandroidsdk.bean
+
+data class WanArticleListBean(
+    var datas:MutableList<WanDataBean>
+)
---
+++ app/src/main/java/com/example/wanandroidsdk/bean/WanDataBean.kt
@@ -0,0 +1,26 @@
+package com.example.wanandroidsdk.bean
+
+data class WanDataBean(
+    var apkLink :String = "",
+    var author: String = "",
+    var chapterId: String = "",
+    var chapterName:String = "",
+    var isCollect:Boolean = false,
+    var courseId:String = "",
+    var desc:String = "",
+    var envelopePic:String = "",
+    var link:String = "",
+    var niceDate:String = "",
+    var origin:String = "",
+    var projectLink:String = "",
+    var publishTime:String = "",
+    var superChapterId:String = "",
+    var superChapterName:String = "",
+    var title:String = "",
+    var type:String = "",
+    var userId:String = "",
+    var zan:String = "",
+    var imagePath:String = "",
+    var url:String = "",
+    var name:String = ""
+)
