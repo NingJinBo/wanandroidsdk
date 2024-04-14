d363fbb45e87360ed19b38b754dba999f8789be0
smile drily <690498789@qq.com>
2024-04-14 12:45:38 +0800

    修改常量工具类为Kotlin

---
+++ app/src/main/java/com/example/wanandroidsdk/constant/Constants.kt
@@ -0,0 +1,22 @@
+package com.example.wanandroidsdk.constant
+
+/**
+ * @auth: njb
+ * @date: 2024/4/14 12:41
+ * @desc: 描述
+ */
+object Constants {
+    const val BASE_URL = "https://www.wanandroid.com"
+
+    const val BANNER_LIST = "/banner/json"
+
+    const val ARTICLE_LIST = "/article/list/{page}/json"
+
+    const val HOT_KEY = "/hotkey/json"
+
+    const val TREE = "/tree/json"
+
+    const val NAVI = "/navi/json"
+
+    const val PROJECT_TREE = "/project/tree/json"
+}
